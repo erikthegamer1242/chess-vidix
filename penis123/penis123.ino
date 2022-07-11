526df28b6f12e0ae546a6987a02712d6e5d0c2d8
LukaGG13 <105482841+LukaGG13@users.noreply.github.com>
2022-07-11 12:54:35 +0200

    Create penis123.ino

---
+++ penis123/penis123.ino
@@ -0,0 +1,85 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
+
+#define TFT_CS 5
+#define TFT_DC 21
+int x_pokazivac =0 ,y_pokazivac = 0,  ud = 35, lr = 34,  UD , LR,x_stari,y_stari;
+
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+
+char board[8][8] = {{'R', 'H', 'C', 'Q', 'K', 'C', 'H', 'R'},
+          {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+          {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+          {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+          {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+          {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+          {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+          {'r', 'h', 'c', 'q', 'k', 'c', 'h', 'r'}};
+
+void draw() {
+   tft.drawRect(40, 0, 240, 240, ILI9341_WHITE);
+
+  for(int i = 30; i < 240; i+=30) {
+    tft.drawFastVLine(i + 40, 0, 240, ILI9341_RED);
+  }
+  for(int i = 30; i < 240; i+=30) {
+    tft.drawFastHLine(40, i, 240, ILI9341_RED);
+  }
+  int boardX=0;
+  int boardY=0;
+
+  for(int j = 0; j < 240; j+=30) {
+    for(int i = 0; i < 240; i+=30) {
+        tft.setCursor(48 + i ,j + 5);
+        tft.print(board[boardY][boardX]);
+        boardX++;
+    }
+    boardX = 0;
+    boardY++;
+  }
+}
+
+void setup() {
+  tft.begin();
+  tft.setRotation(3);
+  tft.fillScreen(ILI9341_BLACK);
+  tft.setTextColor(ILI9341_YELLOW);
+  tft.setTextSize(3.5);
+  tft.setCursor(0, 105);
+  pinMode(ud,INPUT_PULLUP);
+  pinMode(lr,INPUT_PULLUP);
+  draw();
+
+}
+
+void loop() {
+  tft.drawRect((40 + x_pokazivac * 30)+1, (y_pokazivac * 30) + 1 ,29, 28, ILI9341_BLUE);
+  LR = analogRead(lr);
+  UD = analogRead(ud);
+  delay(150);
+  if(UD > 4090)
+  {
+     y_pokazivac--;
+     tft.drawRect((40 + x_stari * 30)+1, (y_stari * 30) + 1 ,29, 28, ILI9341_BLACK);
+  }
+    
+  else if(UD > 1700)
+  {
+    y_pokazivac++;
+    tft.drawRect((40 + x_stari * 30)+1, (y_stari * 30) + 1 ,29, 28, ILI9341_BLACK);
+  }
+    
+  if(LR > 4090)
+  {
+    x_pokazivac--;
+    tft.drawRect((40 + x_stari * 30)+1, (y_stari * 30) + 1 ,29, 28, ILI9341_BLACK);
+  }
+    
+  else if(LR >1700)
+  {
+    x_pokazivac++;
+    tft.drawRect((40 + x_stari * 30)+1, (y_stari * 30) + 1 ,29, 28, ILI9341_BLACK);
+  }
+   x_stari = x_pokazivac;
+   y_stari = y_pokazivac;
+}
